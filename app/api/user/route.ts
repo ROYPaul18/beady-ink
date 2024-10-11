726b93f18eabc844479a9cb3aaa225c7c18265ba
ROYPaul18 <114011395+ROYPaul18@users.noreply.github.com>
2024-10-11 14:38:41 +0200

    Essaie de detecter l'erreur au moment de créer un compte en prod

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -9,12 +9,14 @@ const userSchema = z.object({
   password: z
     .string()
     .min(1, 'Password is required')
-    .min(8, 'Password must have than 8 characters'),
+    .min(8, 'Password must have more than 8 characters'),
 });
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+
+    // Validate request body
     const { email, username, password } = userSchema.parse(body);
 
     // Check if email or username already exists
@@ -31,20 +33,35 @@ export async function POST(req: Request) {
 
     const hashPassword = await hash(password, 10);
 
-    // Créez un nouvel utilisateur avec un rôle par défaut "user"
+    // Create new user with default role 'USER'
     const newUser = await db.user.create({
       data: {
         username,
         email,
         password: hashPassword,
-        role: 'USER', // Rôle par défaut
+        role: 'USER', // Default role
       },
     });
 
+    // Exclude password from the response
     const { password: newUserPassword, ...rest } = newUser;
+
     return NextResponse.json({ user: rest, message: "Utilisateur créé avec succès" }, { status: 201 });
 
-  } catch (error) {
-    return NextResponse.json({ message: "Erreur 500" }, { status: 500 });
+  } catch (error: unknown) {
+    // Handle Zod validation errors separately
+    if (error instanceof z.ZodError) {
+      return NextResponse.json({ message: "Validation Error", errors: error.errors }, { status: 400 });
+    }
+
+    // Check if error is an instance of the built-in Error class
+    if (error instanceof Error) {
+      console.error("Error creating user:", error.message);
+      return NextResponse.json({ message: `Erreur 500: ${error.message}` }, { status: 500 });
+    }
+
+    // Handle any other unknown type of error
+    console.error("Unknown error", error);
+    return NextResponse.json({ message: "Erreur 500: An unknown error occurred" }, { status: 500 });
   }
 }
